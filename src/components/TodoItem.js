d7d5466cde77549432af03eafaf973932859f466
Sinoxolo Sooi <snoxy.sooi@gmail.com>
2021-09-03 00:58:47 +0200

    Todo Ap Commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,69 @@
+import React from "react";
+//module stylesheet
+import styles from "./TodoItem.module.css";
+
+class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  };
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+    //console.log("edit mode activated ");
+  };
+
+  handleUpdateDone = event => {
+    if (event.key === "Enter"){
+      this.setState({editing: false})
+    }
+    //console.log(event.key)
+  }
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    };
+    const { completed, id, title } = this.props.todo;
+    let viewMode = {};
+    let editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = "none";
+    } else {
+      editMode.display = "none";
+    }
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={(e) => {
+            this.props.setUpdate(e.target.value, id);
+          }}
+          onKeyDown={this.handleUpdateDone}
+        />
+      </li>
+    );
+  }
+}
+
+/* function TodoItem(props) {
+  return <li>{props.todo.title}</li>;
+}*/
+
+export default TodoItem;
